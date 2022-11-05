8e9427fea8f340570fee5f382decacd411de144c
kite <stopkite98@naver.com>
2022-11-05 17:38:02 +0900

    [FEAT] #1 - set bottom navi

---
+++ app/src/main/java/com/swu/aos_init/presentation/ui/mypage/MyPageFragment.kt
@@ -0,0 +1,8 @@
+package com.swu.aos_init.presentation.ui.mypage
+
+import com.swu.aos_init.R
+import com.swu.aos_init.databinding.FragmentMypageBinding
+import com.swu.aos_init.presentation.base.BaseFragment
+
+class MyPageFragment : BaseFragment<FragmentMypageBinding>(R.layout.fragment_mypage) {
+}
