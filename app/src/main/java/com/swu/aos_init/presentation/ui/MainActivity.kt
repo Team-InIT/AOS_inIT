fd60d74b5f3be19d1d5026930006594ab13be0bb
kite <stopkite98@naver.com>
2022-11-05 16:46:49 +0900

    [CHORE] 기초 세팅 완료

---
+++ app/src/main/java/com/swu/aos_init/presentation/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.swu.aos_init.presentation.ui
+
+import android.os.Bundle
+import com.swu.aos_init.R
+import com.swu.aos_init.databinding.ActivityMainBinding
+import com.swu.aos_init.presentation.base.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
