6af1ed98d7f2a2792c0b3ce0c95654ec907b6c03
kite <stopkite98@naver.com>
2022-11-06 12:25:46 +0900

    [UI] #6 - add signup activity view

--- app/src/main/java/com/swu/aos_init/presentation/ui/sign/signup/SignUpActivity.kt
+++ app/src/main/java/com/swu/aos_init/presentation/ui/sign/signup/SignUpActivity.kt
@@ -1,4 +1,8 @@
 package com.swu.aos_init.presentation.ui.sign.signup
 
-class SignUpActivity {
+import com.swu.aos_init.R
+import com.swu.aos_init.databinding.ActivitySigninBinding
+import com.swu.aos_init.presentation.base.BaseActivity
+
+class SignUpActivity : BaseActivity<ActivitySigninBinding>(R.layout.activity_signup) {
 }
---
+++ app/src/main/java/com/swu/aos_init/presentation/ui/sign/signup/SignUpViewModel.kt
@@ -0,0 +1,6 @@
+package com.swu.aos_init.presentation.ui.sign.signup
+
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel : ViewModel() {
+}
