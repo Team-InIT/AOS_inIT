da05503b35a250ba9179d34e9c8c1baaec0aaa42
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-11-15 15:47:44 +0900

    [FEAT] #20 엑티비티 추가

---
+++ app/src/main/java/com/swu/aos_init/presentation/ui/home/write/WriteProjectActivity.kt
@@ -0,0 +1,14 @@
+package com.swu.aos_init.presentation.ui.home.write
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.swu.aos_init.R
+import com.swu.aos_init.databinding.ActivityWriteProjectBinding
+import com.swu.aos_init.presentation.base.BaseActivity
+
+class WriteProjectActivity : BaseActivity<ActivityWriteProjectBinding>(R.layout.activity_write_project) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
