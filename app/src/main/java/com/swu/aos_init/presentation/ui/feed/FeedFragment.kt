1012781c08e56bb7f997cfa609ca2c07c7820526
stopkite <stopkite98@naver.com>
2022-11-22 09:50:08 +0900

    [FEAT]#43 - add feed writing layout

--- app/src/main/java/com/swu/aos_init/presentation/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/swu/aos_init/presentation/ui/feed/FeedFragment.kt
@@ -1,5 +1,6 @@
 package com.swu.aos_init.presentation.ui.feed
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.swu.aos_init.R
@@ -7,6 +8,7 @@ import com.swu.aos_init.data.response.feed.ResponseFeed
 import com.swu.aos_init.databinding.FragmentFeedBinding
 import com.swu.aos_init.presentation.base.BaseFragment
 import com.swu.aos_init.presentation.ui.feed.adapter.FeedAdapter
+import com.swu.aos_init.presentation.ui.feed.write.WritingFeedActivity
 
 class FeedFragment : BaseFragment<FragmentFeedBinding>(R.layout.fragment_feed) {
 
@@ -16,6 +18,7 @@ class FeedFragment : BaseFragment<FragmentFeedBinding>(R.layout.fragment_feed) {
         super.onViewCreated(view, savedInstanceState)
 
         initAdapter()
+        moveToFeedWrite()
     }
 
     // TODO: 추후 서버 통신 진행 예정
@@ -55,5 +58,11 @@ class FeedFragment : BaseFragment<FragmentFeedBinding>(R.layout.fragment_feed) {
         )
 
         binding.rvFeed.adapter = feedAdapter
+    }
+
+    private fun moveToFeedWrite() {
+        binding.ivAddFeed.setOnClickListener {
+            startActivity(Intent(requireContext(), WritingFeedActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/swu/aos_init/presentation/ui/feed/write/WritingFeedActivity.kt
+++ app/src/main/java/com/swu/aos_init/presentation/ui/feed/write/WritingFeedActivity.kt
@@ -1,4 +1,21 @@
 package com.swu.aos_init.presentation.ui.feed.write
 
-class WritingFeedActivity {
+import android.os.Bundle
+import com.swu.aos_init.R
+import com.swu.aos_init.databinding.ActivityWriteFeedBinding
+import com.swu.aos_init.presentation.base.BaseActivity
+
+class WritingFeedActivity : BaseActivity<ActivityWriteFeedBinding>(R.layout.activity_write_feed) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initBackBtn()
+    }
+
+    private fun initBackBtn() {
+        binding.ivOpenFeedBack.setOnClickListener {
+            finish()
+        }
+    }
 }
