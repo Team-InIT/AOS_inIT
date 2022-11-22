80bcf9ee052aca39935c3f54fc6c55e9f3002736
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-11-22 13:37:52 +0900

    [FEAT] #45 add clicklistener

--- app/src/main/java/com/swu/aos_init/presentation/ui/mypage/MyPageSettingActivity.kt
+++ app/src/main/java/com/swu/aos_init/presentation/ui/mypage/MyPageSettingActivity.kt
@@ -1,12 +1,39 @@
 package com.swu.aos_init.presentation.ui.mypage
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import com.swu.aos_init.R
+import com.swu.aos_init.databinding.ActivityMyPageSettingBinding
+import com.swu.aos_init.presentation.base.BaseActivity
+import com.swu.aos_init.presentation.ui.sign.signin.SignInActivity
 
-class MyPageSettingActivity : AppCompatActivity() {
+class MyPageSettingActivity : BaseActivity<ActivityMyPageSettingBinding>(R.layout.activity_my_page_setting) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_page_setting)
+        backBtnListener()
+        logoutListener()
+        quitListener()
+    }
+
+    private fun backBtnListener() {
+        binding.imgMypageSettingMovePage.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun logoutListener() {
+        binding.tvMypageLogout.setOnClickListener {
+            finish()
+            val intentSetting = Intent(this, SignInActivity::class.java)
+            startActivity(intentSetting)
+        }
+    }
+
+    private fun quitListener() {
+        binding.tvMypageQuit.setOnClickListener {
+            finish()
+            val intentSetting = Intent(this, SignInActivity::class.java)
+            startActivity(intentSetting)
+        }
     }
 }
