31d32c0d488718bcd1c15991f369743885ee44b5
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-11-24 11:35:45 +0900

    [FEAT] #56 add adapter

---
+++ app/src/main/java/com/swu/aos_init/presentation/ui/mypage/MyPageZzimActivity.kt
@@ -0,0 +1,56 @@
+package com.swu.aos_init.presentation.ui.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.swu.aos_init.R
+import com.swu.aos_init.data.response.ResponseProject
+import com.swu.aos_init.data.response.mypage.ResponseMyPageZzim
+import com.swu.aos_init.databinding.ActivityMyPageZzimBinding
+import com.swu.aos_init.presentation.base.BaseActivity
+import com.swu.aos_init.presentation.ui.home.adapter.ProjectRvAdapter
+import com.swu.aos_init.presentation.ui.mypage.adapter.MyPageZzimAdapter
+
+class MyPageZzimActivity : BaseActivity<ActivityMyPageZzimBinding>(R.layout.activity_my_page_zzim) {
+
+    private lateinit var zzimAdapter: MyPageZzimAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page_zzim)
+
+        initRvAdapter()
+    }
+
+    // 더미 연결
+    private fun initRvAdapter() {
+        zzimAdapter = MyPageZzimAdapter()
+
+        // TODO 추후 서버통신 시 3개의 데이터만 잘라서 받아오기
+        zzimAdapter.submitList(
+            listOf(
+                ResponseMyPageZzim(
+                    "웹",
+                    "네이버 해커톤 같이 하실 분 구해요",
+                    "기획자 1/3  ･  디자이너  2/4  ･  개발자 3/8",
+                    "2022.11.09 ~ 2022.12.09",
+                    "D-3"
+                ), ResponseMyPageZzim(
+                    "웹",
+                    "네이버 해커톤 같이 하실 분 구해요",
+                    "기획자 1/3  ･  디자이너  2/4  ･  개발자 3/8",
+                    "2022.11.09 ~ 2022.12.09",
+                    "D-3"
+                ), ResponseMyPageZzim(
+                    "웹",
+                    "네이버 해커톤 같이 하실 분 구해요",
+                    "기획자 1/3  ･  디자이너  2/4  ･  개발자 3/8",
+                    "2022.11.09 ~ 2022.12.09",
+                    "D-3"
+                )
+            )
+        )
+
+        binding.rvMypageZzim.adapter = zzimAdapter
+
+    }
+}
