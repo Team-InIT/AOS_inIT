2edfdff85720061ff15969c12fece6b96274c942
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-11-29 02:24:35 +0900

    [FEAT] #67 바텀시트 띄우기

---
+++ app/src/main/java/com/swu/aos_init/presentation/ui/home/project/CheckApplicantActivity.kt
@@ -0,0 +1,12 @@
+package com.swu.aos_init.presentation.ui.home.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.swu.aos_init.R
+
+class CheckApplicantActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_applicant)
+    }
+}
