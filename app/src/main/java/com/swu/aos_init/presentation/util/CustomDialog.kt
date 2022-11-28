d8032e882a571de55a0419deb4cda706cebce868
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-11-28 23:57:15 +0900

    [FEAT] #65 add dialog

---
+++ app/src/main/java/com/swu/aos_init/presentation/util/CustomDialog.kt
@@ -0,0 +1,40 @@
+package com.swu.aos_init.presentation.util
+
+import android.app.Dialog
+import android.content.Context
+import android.view.WindowManager
+import android.widget.TextView
+import androidx.annotation.LayoutRes
+import com.swu.aos_init.R
+
+class CustomDialog(private val context: Context, val title: String) {
+    private val dialog = Dialog(context)
+    private lateinit var onClickedListener: ButtonClickListener
+
+    interface ButtonClickListener {
+        fun onClicked(num: Int)
+    }
+
+    fun setOnClickedListener(listener: ButtonClickListener) {
+        onClickedListener = listener
+    }
+
+    fun showChoiceDialog(@LayoutRes layout: Int) {
+        dialog.setContentView(layout)
+        dialog.findViewById<TextView>(R.id.tv_dialog_title).text = title
+        dialog.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window?.setBackgroundDrawableResource(R.drawable.inset_horizontal_58)
+        dialog.show()
+
+        dialog.findViewById<TextView>(R.id.tv_dialog_no).setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.findViewById<TextView>(R.id.tv_dialog_yes).setOnClickListener {
+            onClickedListener.onClicked(1)
+            dialog.dismiss()
+        }
+    }
+}
