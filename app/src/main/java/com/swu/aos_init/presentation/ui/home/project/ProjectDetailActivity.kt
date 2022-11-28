c2efe71d4adb7bd1fc37b933f75df3a9a48a1a2f
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-11-28 22:00:09 +0900

    [FEAT] #58 end

--- app/src/main/java/com/swu/aos_init/presentation/ui/home/project/ProjectDetailActivity.kt
+++ app/src/main/java/com/swu/aos_init/presentation/ui/home/project/ProjectDetailActivity.kt
@@ -9,6 +9,25 @@ import com.swu.aos_init.presentation.base.BaseActivity
 class ProjectDetailActivity : BaseActivity<ActivityProjectDetailBinding>(R.layout.activity_project_detail) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        backBtnListener()
+        likeBtnListener()
+    }
 
+    private fun backBtnListener() {
+        binding.ivOpenProjectBack.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun likeBtnListener() {
+        binding.ivProjectDetailHeart.setOnClickListener {
+            binding.ivProjectDetailHeart.isSelected = !binding.ivProjectDetailHeart.isSelected
+        }
+    }
+
+    private fun applyBtnListener() {
+        binding.btnFinish.setOnClickListener {
+
+        }
     }
 }
