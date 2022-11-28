10a30ac9cdbbcc8bda623d900bdf4dc598b7c29c
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-11-28 13:54:16 +0900

    [FEAT] #58 add project

---
+++ app/src/main/java/com/swu/aos_init/presentation/ui/home/project/ProjectDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.swu.aos_init.presentation.ui.home.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.swu.aos_init.R
+import com.swu.aos_init.databinding.ActivityProjectDetailBinding
+import com.swu.aos_init.presentation.base.BaseActivity
+
+class ProjectDetailActivity : BaseActivity<ActivityProjectDetailBinding>(R.layout.activity_project_detail) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
--- app/src/main/java/com/swu/aos_init/presentation/ui/mypage/MyPageProjectFragment.kt
+++ app/src/main/java/com/swu/aos_init/presentation/ui/mypage/MyPageProjectFragment.kt
@@ -2,14 +2,10 @@ package com.swu.aos_init.presentation.ui.mypage
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.swu.aos_init.R
 import com.swu.aos_init.databinding.FragmentMyPageProjectBinding
 import com.swu.aos_init.presentation.base.BaseFragment
-import com.swu.aos_init.presentation.ui.home.write.WriteProjectActivity
 
 class MyPageProjectFragment : BaseFragment<FragmentMyPageProjectBinding>(R.layout.fragment_my_page_project) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
