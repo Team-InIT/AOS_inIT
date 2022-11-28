ac1bcb8e113d87054281c19ea2966a8432b88be2
heaven00 <69586104+lhb8106@users.noreply.github.com>
2022-11-28 22:35:37 +0900

    [FEAT] #65 add file

---
+++ app/src/main/java/com/swu/aos_init/presentation/ui/mypage/OtherMyPageActivity.kt
@@ -0,0 +1,12 @@
+package com.swu.aos_init.presentation.ui.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.swu.aos_init.R
+
+class OtherMyPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other_my_page)
+    }
+}
